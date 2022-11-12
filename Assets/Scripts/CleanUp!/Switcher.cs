a814c089056a14597eaa710561f7e15cf812a651
faydoly <99545192+faydoly@users.noreply.github.com>
2022-11-13 02:30:49 +0700

    SetUpTime(not finish) + MakeWeaponSwitcher(have some problem)

---
+++ Assets/Scripts/CleanUp!/Switcher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switcher : MonoBehaviour
+{
+    [SerializeField] private GameObject[] weapons;
+
+    private int _weaponQuantity;
+
+    private int _activeWeapon = 0;
+
+    private int _lastWeapon;
+
+    private void Start()
+    {
+        _weaponQuantity = weapons.Length;
+        _lastWeapon = _activeWeapon;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            _lastWeapon = _activeWeapon;
+
+            _activeWeapon++;
+            if (_activeWeapon >= _weaponQuantity) _activeWeapon = 0;
+
+            weapons[_lastWeapon].SetActive(false);
+            weapons[_activeWeapon].SetActive(true);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            _lastWeapon = _activeWeapon;
+
+            _activeWeapon--;
+            if (_activeWeapon <= 0) _activeWeapon = _weaponQuantity - 1;
+
+            weapons[_lastWeapon].SetActive(false);
+            weapons[_activeWeapon].SetActive(true);
+        }
+    }
+}
